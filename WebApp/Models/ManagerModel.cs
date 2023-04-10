0355077e6278448f0fc953f490e7a4c9885c114e
Danielloas <114517047+Danielloas@users.noreply.github.com>
2023-04-10 22:14:25 +0300

    Update Validation in Edit Manager Table

--- WebApp/Models/ManagerModel.cs
+++ WebApp/Models/ManagerModel.cs
@@ -3,10 +3,19 @@
     public class ManagerModel
     {
         public int Id { get; set; }
+
+        //TODO Validation Name
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Enter how you can be contacted.")]
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public bool Deleted { get; set; }
+
+        //TODO Validation Salary
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Enter salary in whole numbers.")]
         public decimal Salary { get; set; }
+
+        //TODO Validation FullName
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Please enter your full name.")]
         public string FullName { get; set; }
     }
 }
